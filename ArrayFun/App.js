294bd662e76d995452edb1b9d6e34b29f3b40957
dvdrtrgn <dvdrtrgn@gmail.com>
2021-08-25 16:31:16 -0500

    Document a little

--- ArrayFun/App.js
+++ ArrayFun/App.js
@@ -1,37 +1,7 @@
 /* global Vue, */
 import 'https://unpkg.com/vue@2';
 import DB from './DB.js';
-
-const arr1 = [
-  ['name', 'id', 'age', 'weight', 'Cool'],
-  ['Susan', '3', '20', '120', true],
-  ['John', '1', '21', '150', true],
-  ['Bob', '2', '23', '90', false],
-  ['Ben', '4', '20', '100', true],
-];
-const arr2 = [
-  ['name', 'id', 'height'],
-  ['Bob', '2', '50'],
-  ['John', '1', '45'],
-  ['Ben', '4', '43'],
-  ['Susan', '3', '48'],
-];
-const arr3 = [
-  ['name', 'id', 'parent'],
-  ['Bob', '2', 'yes'],
-  ['John', '1', 'yes'],
-];
-const junk1 = [
-  ['name', 'id', 'odd'],
-  ['Barb', '11', 'duck'],
-  ['Erik', '12'],
-];
-const junk2 = [
-  ['id', 'name', 'ball'],
-  ['0', 'Foo'],
-  ['12', 'Skittles', 'buster'],
-  ['a', 'Barbara'],
-];
+import { arr1, arr2, arr3, junk1, junk2 } from './tablefrags.js';
 
 const name = 'App';
 export default new Vue({
@@ -40,7 +10,8 @@ export default new Vue({
   components: {},
   data() {
     return {
-      output: '',
+      output: 'nothin',
+      outputfrag: 'nothin',
       table: [],
       arr1,
       arr2,
@@ -51,9 +22,10 @@ export default new Vue({
   },
   methods: {
     addToDB(arr) {
+      this.outputfrag = (arr);
       DB.addTable(arr);
 
-      // this.output = DB.getJson();
+      this.output = DB.getJson();
       this.table = DB.getTable();
 
       console.dir(DB.getCsv());
---
+++ ArrayFun/tablefrags.js
@@ -0,0 +1,38 @@
+export const arr1 = [
+  ['name', 'id', 'age', 'weight', 'Cool'],
+  ['Susan', '3', '20', '120', true],
+  ['John', '1', '21', '150', true],
+  ['Bob', '2', '23', '90', false],
+  ['Ben', '4', '20', '100', true],
+];
+export const arr2 = [
+  ['name', 'id', 'height'],
+  ['Bob', '2', '50'],
+  ['John', '1', '45'],
+  ['Ben', '4', '43'],
+  ['Susan', '3', '48'],
+];
+export const arr3 = [
+  ['name', 'id', 'parent'],
+  ['Bob', '2', 'yes'],
+  ['John', '1', 'yes'],
+];
+export const junk1 = [
+  ['name', 'id', 'odd', 'Cool', 'ball'],
+  ['Barb', '11', 'duck', true],
+  ['Erik', '12', , , 'goof'],
+];
+export const junk2 = [
+  ['id', 'name', 'ball'],
+  ['0', 'Foo'],
+  ['12', 'Skittles', 'buster'],
+  ['a', 'Barbara', 'sports'],
+];
+
+export default {
+  arr1,
+  arr2,
+  arr3,
+  junk1,
+  junk2,
+};

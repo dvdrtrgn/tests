557c3aa9ae5b5cfc20570ee4839364678ec84c23
dvdrtrgn <dvdrtrgn@gmail.com>
2021-09-03 10:15:28 -0500

    Try dynamic argument attributes

--- VueTest/app.js
+++ VueTest/app.js
@@ -1,7 +1,15 @@
 /*global Vue, */
 
+const flip = Math.random() > 0.5;
+
 const RootComponent = {
-  template: '<h1>see console</h1>',
+  template: /*html*/ `<h1 :[attr]="value">see console</h1>`,
+  data() {
+    return {
+      attr: flip ? 'class' : 'style',
+      value: flip ? 'red' : 'font-style: italic;',
+    };
+  },
 };
 
 const app = Vue.createApp(RootComponent);
